21223723a62d45d087c02708513846995a6d1025
Raphael Serougne <35351071+rsrgn@users.noreply.github.com>
2018-05-29 09:43:52 +0200

    Add files via upload

--- Prj_dataviz.R
+++ Prj_dataviz.R
@@ -4,10 +4,13 @@ rm(list = ls())
 #----Loading libraries----
 
 library(tidyverse)
+
+#Cartographie
 library(sf)
-library(rgdal)
+library(SpatialPosition)
 library(cartography)
 
+library(gridExtra)
 
 #----Setting Working Directory----
 
@@ -35,23 +38,22 @@ moy_tot_quantif_2010 <- read.csv2(file="./data/moy_tot_quantif_2010.csv")
 moy_tot_quantif_2011 <- read.csv2(file="./data/moy_tot_quantif_2011.csv")
 moy_tot_quantif_2012 <- read.csv2(file="./data/moy_tot_quantif_2012.csv")
 
-#cartes
-aquiferes <- readOGR(dsn = "./data/Polygone_MasseDEauSouterraine_VEDL2013_FXX-shp"
-                     , layer="PolygMasseDEauSouterraine", stringsAsFactors=FALSE)
-
-pathAE <- "./data/ADMIN-EXPRESS-COG_1-0__SHP__FRA_2017-06-19/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2017-06-19/ADE-COG_1-0_SHP_LAMB93_FR"
-
-dept <- readOGR(dsn = pathAE, layer="DEPARTEMENT", stringsAsFactors=FALSE)
-
-dept2 <- st_read(dsn = pathAE, layer="DEPARTEMENT", stringsAsFactors=FALSE)
-
 # autres dataset
 
 regions <- read.csv2(file="./data/Reg2016.csv")
 departements <- read.csv2(file="./data/depts2016.csv")
 
-#----Sandbox-mesures----
 
+#cartes avec sf
+
+pathAE <- "./data/ADMIN-EXPRESS-COG_1-0__SHP__FRA_2017-06-19/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2017-06-19/ADE-COG_1-0_SHP_LAMB93_FR"
+
+dept2 <- st_read(dsn = pathAE, layer="DEPARTEMENT", stringsAsFactors=FALSE)
+
+
+#----Préparation des données----
+
+#ajout de l'année aux fichiers des mesures
 ma_qp_fm_ttres_pesteso_2007$année <- "2007"
 ma_qp_fm_ttres_pesteso_2008$année <- "2008"
 ma_qp_fm_ttres_pesteso_2009$année <- "2009"
@@ -59,6 +61,7 @@ ma_qp_fm_ttres_pesteso_2010$année <- "2010"
 ma_qp_fm_ttres_pesteso_2011$année <- "2011"
 ma_qp_fm_ttres_pesteso_2012$année <- "2012"
 
+#fusion des mesure dans un df unique
 mesures <- rbind(ma_qp_fm_ttres_pesteso_2007,
                  ma_qp_fm_ttres_pesteso_2008,
                  ma_qp_fm_ttres_pesteso_2009,
@@ -66,48 +69,198 @@ mesures <- rbind(ma_qp_fm_ttres_pesteso_2007,
                  ma_qp_fm_ttres_pesteso_2011,
                  ma_qp_fm_ttres_pesteso_2012)
 
+#calcul de l'écart par rapport à la norme pour toutes les mesures
 mesures$Ecart_norme <- mesures$MA_MOY - mesures$NORME_DCE
 
+#fusion de toutes les moyennes dans un df unique
+moyennes <- rbind(na.omit(moy_tot_quantif_2007),
+                  na.omit(moy_tot_quantif_2008),
+                  na.omit(moy_tot_quantif_2009),
+                  na.omit(moy_tot_quantif_2010),
+                  na.omit(moy_tot_quantif_2011),
+                  na.omit(moy_tot_quantif_2012))
 
-moyennes <- rbind(moy_tot_quantif_2007,
-                  moy_tot_quantif_2008,
-                  moy_tot_quantif_2009,
-                  moy_tot_quantif_2010,
-                  moy_tot_quantif_2011,
-                  moy_tot_quantif_2012)
-
+#Ajout des noms de regions aux fichiers des moyennes
 moyennes <- left_join(moyennes, stations, by= "CD_STATION")
-
 departements <- left_join(departements,regions,by="REGION")
-
 moyennes <- left_join(moyennes, departements, by=c("NUM_DEP"="DEP"))
-
-#noms des région stocké dans NCC.y
 moyennes <- moyennes %>% dplyr::rename(NOM_REG = NCC.y)
 
+#exploration des données de moyennes par stations
+summary(moyennes$MOYPTOT)
+quantile(moyennes$MOYPTOT,0.9992)
+moyennes %>% filter(MOYPTOT > 50) %>% select(ANNEE, MOYPTOT, NOM_REG)
 
+#graphique barplot des moyennes de mesures par stations
+p1 <- ggplot(data=moyennes, aes(x=NOM_REG, y=MOYPTOT, group=NOM_REG) )  
+p1 <- p1 +  geom_boxplot() #+ facet_wrap(~ NOM_REG)
+p1 <- p1 + theme(legend.position = "none")
+p1 <- p1 + theme(axis.text.x=element_text(angle=90))
+p1
 
+#suppression des 4 valeurs supérieurs à 50
+moyennes2 <- moyennes %>% filter(MOYPTOT < 50) 
+grouped_moyennes <- group_by(moyennes2,ANNEE, NOM_REG)
+per_region <- summarise(grouped_moyennes, n_region=n())
+par_region <- na.omit(summarise(grouped_moyennes, moy_reg=mean(MOYPTOT)))
+
+#----graphique evolution mesure pesticides par regions----
+dev.off() 
+png(paste("./graph/graph1.png",sep=""),  width= 2000 , height= 1200 ,res=250)
+
+p1 <- ggplot(data=par_region, aes(y=moy_reg, x=ANNEE, group=NOM_REG, colour=NOM_REG ) )  
+p1 <- p1 +  geom_line(size=1.2)+ facet_wrap(~ NOM_REG)
+p1 <- p1 + theme(legend.position = "none")
+p1 <- p1 + theme(axis.text.x=element_text(angle=90))
+p1 <- p1 + ggtitle("Evolution des mesures de pesticides par régions et par années\ndans les eaux souteraines en France")
+p1 <- p1 + xlab("Années") + ylab("Quantité de pesticides détectée (en µg/l)")
+p1 <- p1 + labs(caption = "Source: Ministère de la Transition écologique et solidaire")
+p1
+dev.off()
+
+#----praparation des donnes carte----
 
 str(mesures[mesures$Ecart_norme > 0 , "MA_MOY"])
 
 stations$CD_STATION <- as.character(stations$CD_STATION) 
-ma_qp_fm_ttres_pesteso_2007$CD_STATION <- as.character(ma_qp_fm_ttres_pesteso_2007$CD_STATION)
-test <- left_join(x = ma_qp_fm_ttres_pesteso_2007,y =  stations, by= "CD_STATION")
+ma_qp_fm_ttres_pesteso_2012$CD_STATION <- as.character(ma_qp_fm_ttres_pesteso_2012$CD_STATION)
 
-nb_me_sup_norm <- as.data.frame(table(test[test$Ecart_norme > 0 , "NUM_DEP"]))
+#ajout des données des stations dans les mesures
+ma_qp_fm_ttres_pesteso_2012 <- left_join(x = ma_qp_fm_ttres_pesteso_2012,y =  stations, by= "CD_STATION")
+grouped_mesures <- group_by(ma_qp_fm_ttres_pesteso_2012, NUM_DEP, LB_PARAMETRE)
+mesures_par_dept <- na.omit(summarise(grouped_mesures, moy_dept=mean(MA_MOY)))
 
 
-dept3 <- left_join(x=dept2 , y=nb_me_sup_norm, by=c("INSEE_DEP"="Var1") )
+
+mtq <- st_read(system.file("shape/martinique.shp", package="cartography"))
+smoothLayer(x = mtq, var = 'P13_POP',
+            span = 4000, beta = 2, breaks = c(0,5000,seq(10000,110000,10000)),
+            mask = mtq, border = NA,
+            col = carto.pal(pal1 = 'wine.pal', n1 = 12),
+            legend.title.txt = "Population\nPotential",
+            legend.pos = "topright", legend.values.rnd = -2)
+propSymbolsLayer(x = mtq, var = "P13_POP", legend.pos = c(690000, 1599950),
+                 legend.title.txt = "Population 2013",
+                 col = NA, border = "#ffffff50")
+layoutLayer(title = "Actual and Potential Popultation in Martinique",
+            author = "INSEE, 2016", sources = "")
+
+
+
+# Plot dentsity of population
+mtq$dens <- mtq$P13_POP / (st_area(mtq) / (1000 * 1000))
+mygrid$densitykm <- mygrid$P13_POP / (mygrid$gridarea / (1000 * 1000))
+
+
+bks <- getBreaks(v = dept_AMPA$moy_dept, method = "q6")
+cols <- carto.pal(pal1 = "taupe.pal", n1 = length(bks)-1)
+opar <- par(mfrow = c(1,2), mar = c(0,0,0,0))
+
+choroLayer(x = dept_AMPA, var = "moy_dept", breaks = bks,
+           border = "burlywood3", col = cols,
+           legend.pos = "topright", legend.values.rnd = 4,
+           legend.title.txt = "Population density")
+
+mygrid <- getGridLayer(x = dept_AMPA, cellsize = 50000 * 50000,
+                       type = "regular", var = "moy_dept")
+
+choroLayer(x = mygrid, var = "moy_dept", breaks = bks,
+           border = "burlywood3", col = cols,
+           #legend.pos = "n", 
+           legend.values.rnd = 4,
+           legend.title.txt = "Population density")
+
+
+
+smoothLayer(x = dept_AMPA, var = 'moy_dept',
+            span = 75000, beta = 2, #breaks = bks,
+            mask = france, border = NA,
+            col = c(rev(carto.pal("green.pal", 3)), carto.pal("orange.pal", 5)),
+            legend.pos = "topright", legend.values.rnd = 4)
+
+points(x=ma_qp_fm_ttres_pesteso_2012$X_FICT_L93,
+       y=ma_qp_fm_ttres_pesteso_2012$Y_FICT_L93,add=T)
+
+
+mesures_par_dept_endrine <- mesures_par_dept %>% filter(LB_PARAMETRE == "Endrine")
+mesures_par_dept_AMPA <- mesures_par_dept %>% filter(LB_PARAMETRE == "AMPA")
+
+dept3 <- left_join(x=dept2 , y=mesures_par_dept_endrine, by=c("INSEE_DEP"="NUM_DEP") )
+dept_AMPA <- left_join(x=dept2 , y=mesures_par_dept_AMPA, by=c("INSEE_DEP"="NUM_DEP") )
+
 
 dev.off()
 
-
-
-choroLayer(dept3 , var = "Freq")
-layoutLayer(title = "Nombre de mesures supérieurs à la norme DCE \npar département pour l'année 2017",
+choroLayer(dept_AMPA , var = "moy_dept")
+layoutLayer(title = "AMPA / dept",
             frame = TRUE, tabtitle = TRUE)
 
+?choroLayer
 
+# list of breaks
+v <- c(0.05, 0.1, 0.2,0.3,0.35, 0.4, 0.45,0.5,1)
+# set a color palette
+cols <- c(rev(carto.pal("green.pal", 4)), carto.pal("orange.pal", 4))
+cols2 <- c(rev(carto.pal("green.pal", 7)), carto.pal("orange.pal", 7))
+
+
+
+mygrid <- getGridLayer(x = dept_AMPA, var = "moy_dept", cellsize = 100000 * 100000, type = "regular")
+bks <- getBreaks(v=mygrid$moy_dept, method = "q6")
+length(bks)
+cols <- carto.pal(pal1 ="red.pal", n1=length(bks)-1)
+choroLayer(mygrid, 
+           var = "moy_dept",
+           col = cols, breaks = bks,
+           legend.values.rnd = 3)
+
+
+
+summary(mygrid$moy_dept)
+plot(dept3$geometry, border = NA, col = NA, bg = "#A6CAE0")
+
+plot(mygrid$geometry)
+
+dept4 <-  as(dept3,"Spatial")
+
+france <- st_union(dept_AMPA)
+france <- as(france,"Spatial")
+plot(france)
+
+# compute & display the potential map
+
+par(mfrow=c(1,2))
+
+
+choroLayer(mygrid, 
+           var = "moy_dept",
+           col = cols2,
+           legend.values.rnd = 3)
+
+
+cols <- c(rev(carto.pal("green.pal", 4)), carto.pal("orange.pal", 4))
+
+smoothLayer(x = dept_AMPA, 
+            var = "moy_dept",
+            #breaks=bks,
+            span = 0.65e+05, beta = 2, mask = france, 
+            resolution = 5000, col = cols, 
+            legend.title.txt = "AMPA", 
+            border = "grey80", lwd = 0.5,
+            legend.values.rnd = 3)
+
+bks <- getBreaks(v=dept_AMPA$moy_dept, method = "q6")
+length(bks)
+cols <- c(rev(carto.pal("green.pal", length(bks)/2 )), carto.pal("orange.pal", length(bks)/2))
+choroLayer(dept_AMPA , 
+           var = "moy_dept",
+           col = cols, breaks = bks,
+           legend.title.txt = "AMPA",
+           legend.values.rnd = 3)
+
+dept_AMPA$area <- st_area(dept_AMPA)
+dept_AMPA$moy_dept_by_m2 <- dept_AMPA$moy_dept / dept_AMPA$area
+choroLayer(dept_AMPA , var = "moy_dept_by_m2",col = cols,legend.title.txt = "AMPA",legend.values.rnd = 15)
 
 #----carte-----
 
